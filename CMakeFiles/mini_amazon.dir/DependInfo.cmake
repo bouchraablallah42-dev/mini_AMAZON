23b0997a8a2edca1d76231072c754331b2964115
Bouchra <bouchra.ablallah.42@edu.uiz.ac.ma>
2026-05-17 12:08:47 +0100

    add CMake

---
+++ CMakeFiles/mini_amazon.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/admin/Desktop/FINAL_P/mini_AMAZON/src/source_files/cart.cpp" "CMakeFiles/mini_amazon.dir/cart.cpp.o" "gcc" "CMakeFiles/mini_amazon.dir/cart.cpp.o.d"
+  "/mnt/c/Users/admin/Desktop/FINAL_P/mini_AMAZON/src/source_files/cartItem.cpp" "CMakeFiles/mini_amazon.dir/cartItem.cpp.o" "gcc" "CMakeFiles/mini_amazon.dir/cartItem.cpp.o.d"
+  "/mnt/c/Users/admin/Desktop/FINAL_P/mini_AMAZON/src/source_files/client.cpp" "CMakeFiles/mini_amazon.dir/client.cpp.o" "gcc" "CMakeFiles/mini_amazon.dir/client.cpp.o.d"
+  "/mnt/c/Users/admin/Desktop/FINAL_P/mini_AMAZON/src/source_files/clothes.cpp" "CMakeFiles/mini_amazon.dir/clothes.cpp.o" "gcc" "CMakeFiles/mini_amazon.dir/clothes.cpp.o.d"
+  "/mnt/c/Users/admin/Desktop/FINAL_P/mini_AMAZON/src/source_files/electronic.cpp" "CMakeFiles/mini_amazon.dir/electronic.cpp.o" "gcc" "CMakeFiles/mini_amazon.dir/electronic.cpp.o.d"
+  "/mnt/c/Users/admin/Desktop/FINAL_P/mini_AMAZON/src/source_files/food.cpp" "CMakeFiles/mini_amazon.dir/food.cpp.o" "gcc" "CMakeFiles/mini_amazon.dir/food.cpp.o.d"
+  "/mnt/c/Users/admin/Desktop/FINAL_P/mini_AMAZON/src/source_files/main.cpp" "CMakeFiles/mini_amazon.dir/main.cpp.o" "gcc" "CMakeFiles/mini_amazon.dir/main.cpp.o.d"
+  "/mnt/c/Users/admin/Desktop/FINAL_P/mini_AMAZON/src/source_files/order.cpp" "CMakeFiles/mini_amazon.dir/order.cpp.o" "gcc" "CMakeFiles/mini_amazon.dir/order.cpp.o.d"
+  "/mnt/c/Users/admin/Desktop/FINAL_P/mini_AMAZON/src/source_files/product.cpp" "CMakeFiles/mini_amazon.dir/product.cpp.o" "gcc" "CMakeFiles/mini_amazon.dir/product.cpp.o.d"
+  "/mnt/c/Users/admin/Desktop/FINAL_P/mini_AMAZON/src/source_files/store.cpp" "CMakeFiles/mini_amazon.dir/store.cpp.o" "gcc" "CMakeFiles/mini_amazon.dir/store.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/mini_amazon.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mini_amazon.dir/cart.cpp.o"
+  "CMakeFiles/mini_amazon.dir/cart.cpp.o.d"
+  "CMakeFiles/mini_amazon.dir/cartItem.cpp.o"
+  "CMakeFiles/mini_amazon.dir/cartItem.cpp.o.d"
+  "CMakeFiles/mini_amazon.dir/client.cpp.o"
+  "CMakeFiles/mini_amazon.dir/client.cpp.o.d"
+  "CMakeFiles/mini_amazon.dir/clothes.cpp.o"
+  "CMakeFiles/mini_amazon.dir/clothes.cpp.o.d"
+  "CMakeFiles/mini_amazon.dir/electronic.cpp.o"
+  "CMakeFiles/mini_amazon.dir/electronic.cpp.o.d"
+  "CMakeFiles/mini_amazon.dir/food.cpp.o"
+  "CMakeFiles/mini_amazon.dir/food.cpp.o.d"
+  "CMakeFiles/mini_amazon.dir/main.cpp.o"
+  "CMakeFiles/mini_amazon.dir/main.cpp.o.d"
+  "CMakeFiles/mini_amazon.dir/order.cpp.o"
+  "CMakeFiles/mini_amazon.dir/order.cpp.o.d"
+  "CMakeFiles/mini_amazon.dir/product.cpp.o"
+  "CMakeFiles/mini_amazon.dir/product.cpp.o.d"
+  "CMakeFiles/mini_amazon.dir/store.cpp.o"
+  "CMakeFiles/mini_amazon.dir/store.cpp.o.d"
+  "mini_amazon"
+  "mini_amazon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mini_amazon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
